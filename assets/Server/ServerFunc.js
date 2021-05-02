330f73ea7f2108a5163a3d5ab61d13bb2b08d7ae
n4no <74839733+Preauthorized@users.noreply.github.com>
2021-05-01 21:25:31 -0400

    Update ServerFunc.js

--- assets/Server/ServerFunc.js
+++ assets/Server/ServerFunc.js
@@ -7,3 +7,7 @@
 exports.set_title = function(string, socket) {
     socket.write("\033]0;" + string + "\007")
 }
+
+exports.set_TerminalSize = function(row, col, socket) {
+    socket.write("\033[8;" + row + ";" + col + "t");
+}
