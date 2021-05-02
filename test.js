b10f3c89c9bdf2f08613512c4a6982b4bd1f5fcd
n4no <74839733+Preauthorized@users.noreply.github.com>
2021-05-01 21:25:40 -0400

    Update test.js

--- test.js
+++ test.js
@@ -2,6 +2,8 @@
 const Auth = require("./assets/Auth/crud.js");
 const Login = require("./assets/Auth/main.js");
 
+const Extra = require("./assets/Extra/main.js");
+
 let lul = process.argv;
 
 // console.log(Auth.addUser(lul[2], lul[3], lul[4], lul[5], lul[6]))
@@ -10,14 +12,16 @@ let lul = process.argv;
 // Auth.LogSession(lul[2], lul[3]);
 //console.log(Auth.removeSession(lul[2]))
 
-async function niggered() {
-    skid = await Auth.GetCurrentUser("root", "arr");
-    console.log(skid);
-    ggay = await Auth.GetUser("root", "arr")
-    console.log(ggay)
-    // console.log(await Auth.updateUser(lul[2], lul[3], lul[4], lul[5]))
-    // console.log(await Auth.GetCurrentUser(lul[2], "str"));
-    // console.log(await Login.Login(lul[2], lul[3], lul[4]));
-}
+// async function niggered() {
+//     skid = await Auth.GetCurrentUser("root", "arr");
+//     console.log(skid);
+//     ggay = await Auth.GetUser("root", "arr")
+//     console.log(ggay)
+//     // console.log(await Auth.updateUser(lul[2], lul[3], lul[4], lul[5]))
+//     // console.log(await Auth.GetCurrentUser(lul[2], "str"));
+//     // console.log(await Login.Login(lul[2], lul[3], lul[4]));
+// }
 
-niggered();
+// niggered();
+
+Extra.pScan(lul[2]);
