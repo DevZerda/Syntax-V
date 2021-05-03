0e046389d25a635cad16fed6fffdfa6c5ad63eb4
n4no <74839733+Preauthorized@users.noreply.github.com>
2021-05-03 14:32:21 -0400

    Update stresser.js

--- assets/Commands/stresser.js
+++ assets/Commands/stresser.js
@@ -1,9 +1,15 @@
 // Modules
-
+const C = require("../Config/current.js").CurrentCmd;
 // Files
 
 // Extra
 
-exports.Stresser = function(host, port, time, method) {
-    
+exports.Stresser = function(socket) {
+    let argCount = (C.arg).length;
+
+    if(argCount < 4 || argCount > 4) {
+        return "[x] Error, Invalid argument!";
+    }
+
+    socket.write("lul");
 }
