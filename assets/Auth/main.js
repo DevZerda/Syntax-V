40c6722b60ac9da903f1da53353eae2003d7e84b
n4no <74839733+Preauthorized@users.noreply.github.com>
2021-05-15 13:07:50 -0400

    Update main.js

--- assets/Auth/main.js
+++ assets/Auth/main.js
@@ -3,13 +3,39 @@
 // Files
 const Crud = require("./crud");
 
-exports.Login = async function(user, pw, ip) {
-    let acctInfo = await Crud.GetUser(user, "arr");
-    console.log(acctInfo);
-    if(acctInfo === "[x] Error, No user found!") return acctInfo;
-    // if(acctInfo[1] === "none") Extra.changeIP(user);
-    let response = ((acctInfo[0] === user && acctInfo[2] === pw) ? "[x] Successfully logged in. Welcome: " + user + "\r\n" : "[x] Error, Failed");
-    // if(Crud.GetCurrentUser(ip, "str")) return "[x] Error, This user is already signed in. One connection per user!\r\n";
-    Crud.LogSession(user, ip);
-    return response;
+// exports.Login = async function(user, pw, ip) {
+//     let acctInfo = await Crud.GetUser(user, "arr");
+//     console.log(acctInfo);
+//     if(acctInfo === "[x] Error, No user found!") return acctInfo;
+//     // if(acctInfo[1] === "none") Extra.changeIP(user);
+//     let response = ((acctInfo[0] === user && acctInfo[2] === pw) ? "[x] Successfully logged in. Welcome: " + user + "\r\n" : "[x] Error, Failed");
+//     // if(Crud.GetCurrentUser(ip, "str")) return "[x] Error, This user is already signed in. One connection per user!\r\n";
+    
+//     return response;
+// }
+
+exports.login = async function(user, pass, ip) {
+    if(user === "undefined" || pass === "undefined" || ip === "undefined") return "[x] Error, Invalud argument value!";
+
+    let get_user = await Crud.GetUser(user, "arr");
+    console.log(get_user);
+    if(get_user === "[x] Error, No user found!") return "[x] Error, Username or password seem to be incorrect!. Try again";
+
+    let info = get_user;
+
+
+    console.log("Username Input: " + user);
+    console.log("Password Input: " + pass);
+    console.log("Current IP: " + ip);
+    console.log("Closest matching Username Fetched from DB: " + get_user[0]);
+    console.log("Closest matching Password Fetched from DB: " + get_user[2]);
+    console.log("IP Fetched from DB: " + get_user[1]);
+
+
+    let depend = ((user === get_user[0] && pass === get_user[2]) ? "[+] Successfully logged in, Welcome: " + user : "[x] Error, Username or password seem to be incorrect!. Try again");
+    // ((info[1] === "none") ? Crud.changeIP(user, ip) : 0); // Change IP to current if IP equals to 'none'
+    (depend == "[+]" ? Crud.LogSession(user, ip) : 0); // Log Session
+    (depend == "[+]" ? eExtra.log_login(user, ip) : 0); // Log Login
+    console.log(depend);
+    return depend;
 }
