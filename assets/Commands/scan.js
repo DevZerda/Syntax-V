2b47d999e65cf67c93e86d08d40064ae2a8440b3
n4no <74839733+Preauthorized@users.noreply.github.com>
2021-05-16 16:54:37 -0400

    Create scan.js

---
+++ assets/Commands/scan.js
@@ -0,0 +1,13 @@
+// Modules
+const fs = require("fs");
+
+// Files
+const Banner = require("../banner_system/main.js");
+const Extra = require("../Extra/main.js");
+const C = require("../Config/current.js").CurrentCmd;
+
+// Extra
+
+exports.PortScan = function() {
+    
+}
