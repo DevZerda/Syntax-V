97f36f75001cc4ca0bdfc687a98721a708280bde
n4no <74839733+Preauthorized@users.noreply.github.com>
2021-05-16 18:33:22 -0400

    Update scan.js

--- assets/Commands/scan.js
+++ assets/Commands/scan.js
@@ -1,13 +1,37 @@
 // Modules
 const fs = require("fs");
+const f = require("node-fetch");
 
 // Files
 const Banner = require("../banner_system/main.js");
 const Extra = require("../Extra/main.js");
 const C = require("../Config/current.js").CurrentCmd;
+const Config = require("../Config/main.js");
 
 // Extra
 
-exports.PortScan = function() {
-    
+exports.PortScan = async function() {
+    let getPorts = await(await f(`https://api.hackertarget.com/nmap/?q=${C.arg[1]}`)).text();
+    let results = "";
+    getPorts.split("\n").forEach(e => {
+        if(e.includes("open")) {
+            results += "                    " + ((((e.split(" ").join(",")).split(",,,").join(" ")).split(",,").join(",")).split(",").join(" ").split(" ") + "\r\n");
+        } else if(e.includes("closed")) {
+            results += "                    " + ((((e.split(" ").join(",")).split(",,,").join(" ")).split(",,").join(",")).split(",").join(" ").split(" ") + "\r\n");
+        } else if(e.includes("filter")) {
+            results += "                    " + ((((e.split(" ").join(",")).split(",,,").join(" ")).split(",,").join(",")).split(",").join(" ").split(" ") + "\r\n");
+        }
+    })
+    return this.ColorPortScan(results)
+}
+
+exports.ColorPortScan = function(r) {    
+    lines = r.split("\n");
+    let results = "";
+    lines.forEach(e => {
+        if(e.length < 4) return;
+        let shit = e.split(",");
+        results += Config.Colors.Red + shit[0] + " " + Config.Colors.Yellow + shit[1] + " " + Config.Colors.Red + shit[2] + "\n";
+    })
+    return results;
 }
